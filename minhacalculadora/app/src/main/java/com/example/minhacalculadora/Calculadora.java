70078c937896c6db19a240ca52ed50e07e0f1c99
2001Eduardo <61715159+2001eduardo@users.noreply.github.com>
2020-03-05 22:30:28 -0300

    inicio de projeto SegPartProjCalculadora

---
+++ minhacalculadora/app/src/main/java/com/example/minhacalculadora/Calculadora.java
@@ -0,0 +1,5 @@
+package com.example.minhacalculadora;
+
+public class Calculadora {
+
+}
---
+++ minhacalculadora/app/src/main/java/com/example/minhacalculadora/Operacao.java
@@ -0,0 +1,10 @@
+package com.example.minhacalculadora;
+
+public class Operacao {
+
+
+    public void soma(Valor valor) {
+        valor.setResultado(valor.getValor1() + valor.getValor2());
+    }
+}
+
